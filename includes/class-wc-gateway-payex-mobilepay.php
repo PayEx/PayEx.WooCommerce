f203c212652af1a0e2842b2a66047a0306d51de8
Oleg Iskusnyh <oleg@iskusnyh.pro>
2018-09-18 13:17:21 +0600

    Reduce stock on Authorization

--- includes/class-wc-gateway-payex-mobilepay.php
+++ includes/class-wc-gateway-payex-mobilepay.php
@@ -325,10 +325,20 @@ class WC_Gateway_Payex_Mobilepay extends WC_Gateway_Payex_Abstract {
 				WC()->cart->empty_cart();
 				break;
 			case 1:
+				$order_stock_reduced = $this->is_wc3() ? $order->get_meta( '_order_stock_reduced', true ) : get_post_meta( $order_id, '_order_stock_reduced', true );
+				if ( ! $order_stock_reduced ) {
+					$this->is_wc3() ? wc_reduce_stock_levels( $order_id ) : $order->reduce_order_stock();
+				}
+
 				$order->update_status( 'on-hold', sprintf( __( 'Transaction is pending. Transaction Id: %s', 'woocommerce-gateway-payex-payment' ), $result['transactionNumber'] ) );
 				WC()->cart->empty_cart();
 				break;
 			case 3:
+				$order_stock_reduced = $this->is_wc3() ? $order->get_meta( '_order_stock_reduced', true ) : get_post_meta( $order_id, '_order_stock_reduced', true );
+				if ( ! $order_stock_reduced ) {
+					$this->is_wc3() ? wc_reduce_stock_levels( $order_id ) : $order->reduce_order_stock();
+				}
+
 				$order->update_status( 'on-hold', sprintf( __( 'Transaction authorized. Transaction Id: %s', 'woocommerce-gateway-payex-payment' ), $result['transactionNumber'] ) );
 				WC()->cart->empty_cart();
 				break;
